5a7365cbb2d9bc053159158c9d03059d0e21ecce
ghawman <ghawman@brb-10457.apn.wlan.private.upenn.edu>
2023-06-22 17:05:07 -0400

    still not working but closer

--- war_card_game/src/App.jsx
+++ war_card_game/src/App.jsx
@@ -2,14 +2,15 @@ import { useRef, useState, useEffect } from 'react';
 import './App.css';
 import CardFlip from './cardFlip';
 // import Deck from './deck.js';
-// import War from './war';
+import War from './war';
 
 function App() {
     
 
     return (
         <>
-            <CardFlip />
+            {/* <CardFlip /> */}
+            <War />
         </>
     )
 }
--- war_card_game/src/war.jsx
+++ war_card_game/src/war.jsx
@@ -3,21 +3,16 @@ import './App.css';
 import Deck from './deck.js';
 
 function War() {
-    const [playerDeck, setPlayerDeck] = useState([])
-    const [computerDeck, setComputerDeck] = useState([])
+    const [playerDeck, setPlayerDeck] = useState(null)
+    const [playerCardSlot, setPlayerCardSlot] = useState(null)
+    const [computerDeck, setComputerDeck] = useState(null)
+    const [computerCardSlot, setComputerCardSlot] = useState(null)
     const [inRound, setInRound] = useState(false)
     const [stop, setStop] = useState(false)
     const [war, setWar] = useState(false)
     const [text, setText] = useState('')
 
-    let deck = new Deck
-    console.log(deck.cards.slice(0,3))
-
-    useEffect(() => {
-        startGame(); // Call startGame function when the component is mounted
-    }, []);
-
-    console.log(playerDeck)
+    const deck = new Deck
 
     const CARD_VALUE_MAP = {
         "2" : 2,
@@ -35,15 +30,13 @@ function War() {
         "A" : 14,
     }
 
-    const computerCardSlot = useRef(null);
-    console.log(computerCardSlot)
+    useEffect(() => {
+        startGame(); // Call startGame function when the component is mounted
+    }, []);
 
-    const computerDeckElement = useRef(null);
-    const playerCardSlot = useRef(null);
-    const playerDeckElement = useRef(null);
+
 
     function startGame() {
-        const deck = new Deck();
         deck.shuffle()
 
         const deckMidPoint = Math.ceil(deck.numberOfCards / 2)
@@ -58,8 +51,8 @@ function War() {
 
     function cleanBeforeRound() {
         setInRound(false)
-        computerCardSlot.innerHTML = ''
-        playerCardSlot.innerHTML = ''
+        setComputerCardSlot(null)
+        setPlayerCardSlot(null)
         setText('')
 
         updateDeckCount()
@@ -68,32 +61,27 @@ function War() {
     function flipCards() {
         setInRound(true)
 
-        const updatedPlayerDeck = [...playerDeck];
-        const updatedComputerDeck = [...computerDeck];
-
         const playerCard = playerDeck.pop()
         const computerCard = computerDeck.pop()
+        console.log(playerCard)
 
-        playerCardSlot.current.innerHTML = getCardHTML(playerCard);
-        computerCardSlot.current.innerHTML = getCardHTML(computerCard);
+        setPlayerCardSlot(playerCard);
+        setComputerCardSlot(computerCard);
 
         updateDeckCount()
-
+        
         if (isRoundWinner(playerCard, computerCard)) {
             setText('Win')
-            updatedPlayerDeck.push(playerCard, computerCard);
-            setPlayerDeck(updatedPlayerDeck);
-            setComputerDeck(updatedComputerDeck);
+            addCardsToPlayerDeck(playerCard, computerCard)
+
         } else if (isRoundWinner(computerCard, playerCard)){
             setText("Lose");
-            updatedComputerDeck.push(computerCard, playerCard);
-            setPlayerDeck(updatedPlayerDeck);
-            setComputerDeck(updatedComputerDeck);
+            addCardsToComputerDeck(computerCard, playerCard)
         } else {
             setText('War')
             warRound()
         }
-
+        
         if (isGameOver(playerDeck)) {
             setText('You Lose!!!')
             setStop(true)
@@ -103,12 +91,22 @@ function War() {
         }
     }
 
+    function addCardsToPlayerDeck(cardOne, cardTwo){
+        setPlayerDeck((oldDeck) => (oldDeck === null ? [cardOne, cardTwo] : [...oldDeck, cardOne, cardTwo]));
+    }
+    function addCardsToComputerDeck(cardOne, cardTwo){
+        setComputerDeck((oldDeck) => (oldDeck === null ? [cardOne, cardTwo] : [...oldDeck, cardOne, cardTwo]));
+    }
+
     function updateDeckCount() {
-        computerDeckElement.current.innerText = computerDeck.numberOfCards
-        playerDeckElement.current.innerText = playerDeck.numberOfCards
+        if (computerDeck !== null && playerDeck !== null) {
+            computerDeck.numberOfCards
+            playerDeck.numberOfCards
+        }
     }
 
     function isRoundWinner(cardOne, cardTwo) {
+        console.log(cardOne, cardTwo)
         return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
     }
 
@@ -121,26 +119,31 @@ function War() {
         const computerTribute = computerDeck.cards.slice(0,3)
     }
 
-    const getCardHTML = (card) => {
-        return (
-        <div className={`card ${card.color}`}>
-            {card.suit}
-        </div>
-        );
-    };
-
     return (
-        <>
-            <div className='computer-deck deck' ref={computerDeckElement}></div>
-            <div className='computer-card-slot card slot' ref={computerCardSlot}></div>
-            <div className='text'>{text}</div>
-            <div className='player-deck deck' ref={playerDeckElement}></div>
-            <div className='player-card-slot card-slot' ref={playerCardSlot}></div>
-            <button onClick={stop ? startGame : (inRound ? cleanBeforeRound : flipCards)}>
-                {stop ? 'Start Game' : (inRound ? 'Clean Before Round' : 'Flip Cards')}
-            </button>
-            <div className='box'>hi</div>
-        </>
+        <div className='war-board'>
+            <div>
+                <div>Computer</div>
+                {computerDeck === null ? <div className='computer-deck deck'></div> : <div className='computer-deck deck'>{computerDeck.numberOfCards}</div>}
+                {computerCardSlot === null ? 
+                    <div className='computer-card-slot card-slot'></div> : 
+                    <div className='computer-card-slot card-slot'>{computerCardSlot.value}{computerCardSlot.suit}</div>
+                }
+            </div>
+            <div>
+                <div className='text'>{text}</div>
+                <button className='war-button' onClick={stop ? startGame : (inRound ? cleanBeforeRound : flipCards)}>
+                    {stop ? 'Start Game' : (inRound ? 'Clear' : 'Flip Cards')}
+                </button>
+            </div>
+            <div>
+                <div>Player</div>
+                {playerCardSlot === null ? 
+                    <div className='player-card-slot card-slot'></div> : 
+                    <div className='player-card-slot card-slot'>{playerCardSlot.value}{playerCardSlot.suit}</div>
+                }
+                {playerDeck === null ? <div className='player-deck deck'></div> : <div className='player-deck deck'>{playerDeck.numberOfCards}</div>}
+            </div>
+        </div>
     )
 }
 
