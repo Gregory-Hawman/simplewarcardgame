6b027ee9896e6d96f88fefc53a2b8b0aaadb155b
ghawman <ghawman@brb-10457.apn.wlan.private.upenn.edu>
2023-07-03 15:59:02 -0400

    trying to refactor the the functions so they truly only do one thing.

--- war_card_game/src/deck.js
+++ war_card_game/src/deck.js
@@ -1,27 +1,5 @@
-const SUITS = ["♠", "♣", "♥", "♦"]
-const VALUES = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A",
-]
-
-const SHORT_VALUES = [
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A",
-]
+import {SUITS} from './vars';
+import {VALUES} from './vars';
 
 export default class Deck{
     constructor(cards = freshDeck()) {
@@ -63,7 +41,7 @@ class Card {
 
 function freshDeck() {
     return SUITS.flatMap(suit => {
-        return SHORT_VALUES.map(value => {
+        return VALUES.map(value => {
             return new Card(suit, value)
         })
     })
--- war_card_game/src/war.jsx
+++ war_card_game/src/war.jsx
@@ -1,83 +1,114 @@
 import { useRef, useState, useEffect } from 'react';
 import './App.css';
 import Deck from './deck.js';
+import { ACE_HIGH_CARD_VALUE_MAP } from './vars';
 
 function War() {
+    // player displayed states
     const [playerDeck, setPlayerDeck] = useState()
     const [playerCardSlot, setPlayerCardSlot] = useState(null)
     const [playerWarTribute, setPlayerWarTribute] = useState(new Deck([]))
 
+    // computer displayed states
     const [computerDeck, setComputerDeck] = useState()
     const [computerCardSlot, setComputerCardSlot] = useState(null)
     const [computerWarTribute, setComputerWarTribute] = useState(new Deck([]))
 
-    const [inRound, setInRound] = useState(false)
-    const [stopGame, setStopGame] = useState(false)
-
-    const [startWar, setStartWar] = useState(false)
-    const [warRoundCount, setWarRoundCount] = useState(0)
-    const [tributeSize, setTributeSize] = useState(null)
-    const [warSwitch, setWarSwitch] = useState(false)
-
+    //other displayed states
     const [text, setText] = useState('')
 
-    const deck = new Deck()
+    // condition states
+    const [inRound, setInRound] = useState(false)
+    const [stopGame, setStopGame] = useState(false)
+    const [tributeSize, setTributeSize] = useState(null)
 
-    const CARD_VALUE_MAP = {
-        "2" : 2,
-        "3" : 3,
-        "4" : 4,
-        "5" : 5,
-        "6" : 6,
-        "7" : 7,
-        "8" : 8,
-        "9" : 9,
-        "10" : 10,
-        "J" : 11,
-        "Q" : 12,
-        "K" : 13,
-        "A" : 14,
-    }
+    //trigger states
+    const [startNewWar, setStartNewWar] = useState(false)
+    const [warCount, setWarCount] = useState(0)
+    const [warSwitch, setWarSwitch] = useState(false)
+    const [checkingWinner, setCheckingWinner] = useState(false)
+
+
+    const deck = new Deck()
 
     useEffect(() => {
         startGame(); // Call startGame function when the component is mounted
     }, []);
 
     useEffect(() => {
-        if(warRoundCount > 0){
+        // only add cards to the tribute decks once the tribute size has been set
+        if(warCount > 0){
             for (let i = 0; i < tributeSize; i++){
                 const playerCard = playerDeck.pop()
                 const computerCard = computerDeck.pop()
                 addCardsToDeck('playerTribute', playerCard)
                 addCardsToDeck('computerTribute', computerCard)
+
+                // changing the amount of cards in the main decks so update the count
                 updateDeckCount()
             }
         }
-    },[warSwitch])
+    // warSwitch instead of tribute size because war may be called twice in a row with the same tribute size, so gettting the tribute size will just always flip the warSwitch as well, thus triggering here
+    }, [warSwitch])
+
+    useEffect(() => {
+        if (warCount > 0) {
+            if (playerCardSlot !== null && computerCardSlot !== null){
+                if (isRoundWinner(playerCardSlot, computerCardSlot)) {
+                    setText('You Win the War')
+                    addCardsToDeck('player', playerCardSlot, computerCardSlot, ...playerWarTribute.cards, ...computerWarTribute.cards)
+                } else if (isRoundWinner(computerCardSlot, playerCardSlot)){
+                    setText("You Lose the War");
+                    addCardsToDeck('computer', computerCardSlot, playerCardSlot, ...computerWarTribute.cards, ...playerWarTribute.cards)
+                } else {
+                    setText('War Again')
+                    setStartNewWar(true)
+                }
+            }
+        } else {
+            if (playerCardSlot !== null && computerCardSlot !== null){
+                if (isRoundWinner(playerCardSlot, computerCardSlot)) {
+                    setText('You Win')
+                    addCardsToDeck('player', playerCardSlot, computerCardSlot)
+        
+                } else if (isRoundWinner(computerCardSlot, playerCardSlot)){
+                    setText("You Lose");
+                    addCardsToDeck('computer', computerCardSlot, playerCardSlot)
+        
+                } else {
+                    setText('War')
+                    setStartNewWar(true)
+                }
+            }
+        }
+    }, [checkingWinner])
 
     function startGame() {
+        // shuffle the cards
         deck.shuffle()
 
+        // split the deck in half and give one to each player
         const deckMidPoint = Math.ceil(deck.numberOfCards / 2)
         setPlayerDeck(new Deck(deck.cards.slice(0, deckMidPoint)))
         setComputerDeck(new Deck(deck.cards.slice(deckMidPoint, deck.numberOfCards)))
+
+        // set the game to default status. mostly for playing again once the last game is over.
         setInRound(false),
         setStopGame(false)
-        setStartWar(false)
-
+        setStartNewWar(false)
         cleanBeforeRound()
     }
 
     function cleanBeforeRound() {
+        // clear the last flipped cards out
         setComputerCardSlot(null)
         setPlayerCardSlot(null)
 
-        if (warRoundCount > 0 && startWar) {
-            setText('War')
-        } else if (warRoundCount > 0 && startWar === false) {
+        // if we are in a war warCount will be > 0, if we don't get a second war, war count will still be > 0 but will have to be reset
+        if (warCount > 0 && startNewWar === false) {
             setPlayerWarTribute(new Deck([]))
             setComputerWarTribute(new Deck([]))
-            setWarRoundCount(0)
+            setWarCount(0)
             setInRound(false)
             setText('')
             setTributeSize(null)
@@ -85,21 +116,10 @@ function War() {
             setInRound(false)
             setText('')
         }
-        if(playerDeck && computerDeck){
-            if (warRoundCount === 0){
-                if (isGameOver(playerDeck)) {
-                    setText('You Lose!!!')
-                    setStopGame(true)
-        
-                } else if (isGameOver(computerDeck)) {
-                    setText('You Won!')
-                    setStopGame(true)    
-                }
-            }
-        }
     }
 
     function updateDeckCount() {
+        // as long as both the players deck and the computers deck have been delt cards, count the cards
         if (computerDeck !== undefined && playerDeck !== undefined) {
             computerDeck.numberOfCards
             playerDeck.numberOfCards
@@ -111,54 +131,28 @@ function War() {
     }
 
     function flipCards() {
-        setInRound(true)
+        // fliping the top deck card into the respective card slot
 
         let playerCard
         let computerCard
 
-        if (playerDeck.numberOfCards === 0){
+        // if we have gotten here and the player has no cards left in their deck, it should mean there is an ongoing warPhase and the player needs to keep playing with the last card flipped
+        if (playerDeck.numberOfCards === 0 && startNewWar === true){
             playerCard = playerCardSlot
-        } else if (computerDeck.numberOfCards === 0) {
+        } else if (computerDeck.numberOfCards === 0 && startNewWar === true) {
             computerCard = computerCardSlot
-        }else {
+        } else {
             playerCard = playerDeck.pop()
             computerCard = computerDeck.pop()
         }
         
         setPlayerCardSlot(playerCard);
         setComputerCardSlot(computerCard);
-
         updateDeckCount()
-
-        if (warRoundCount > 0) {
-            if (isRoundWinner(playerCard, computerCard)) {
-                setText('You Win the War')
-                addCardsToDeck('player', playerCard, computerCard, ...playerWarTribute.cards, ...computerWarTribute.cards)
-            } else if (isRoundWinner(computerCard, playerCard)){
-                setText("You Lose the War");
-                addCardsToDeck('computer', computerCard, playerCard, ...computerWarTribute.cards, ...playerWarTribute.cards)
-            } else {
-                setText('War Again')
-                setStartWar(true)
-            }
-        } else {
-            if (isRoundWinner(playerCard, computerCard)) {
-                setText('You Win')
-                addCardsToDeck('player', playerCard, computerCard)
-    
-            } else if (isRoundWinner(computerCard, playerCard)){
-                setText("You Lose");
-                addCardsToDeck('computer', computerCard, playerCard)
-    
-            } else {
-                setText('War')
-                setStartWar(true)
-            }
-        }
     }
 
     function isRoundWinner(cardOne, cardTwo) {
-        return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
+        return ACE_HIGH_CARD_VALUE_MAP[cardOne.value] > ACE_HIGH_CARD_VALUE_MAP[cardTwo.value]
     }
 
     function addCardsToDeck (deckType, ...cards) {
@@ -189,13 +183,14 @@ function War() {
         }
     }
 
-    function warRound() {
-        setInRound(true)
-        setStartWar(false)
-        setWarRoundCount(warRoundCount + 1)
+    // war phase is just adding the extra cards up for grabs
+    function warPhase() {
+        // War has started already.
+        setStartNewWar(false)
 
-        // store the war causing card in the tribute pile face up
+        // store the war causing card in the tribute deck
         if (playerDeck.numberOfCards === 0){
+            // if the players deck is empty after the flip and it caused a war phase only move the computer card to their tribute
             addCardsToDeck('computerTribute', computerCardSlot)
         } else if (computerDeck.numberOfCards === 0){
             addCardsToDeck('playerTribute', playerCardSlot)
@@ -214,22 +209,69 @@ function War() {
         } else {
             setTributeSize(0)
         }
-        // The tributeSize state change triggers the useEffect for loop at the top to add cards to the to the respective warTributeDecks.
-
-        // clear the board
-        cleanBeforeRound()
+        
+        // The warSwitch state change triggers the useEffect for loop at the top to add cards to the to the respective warTributeDecks.
         setWarSwitch(!warSwitch)
     }
 
+
+    function fullRound() {
+        // we are now in a round
+        setInRound(true)
+
+        // can we keep playing
+        // do the players deck and computer deck both have cards left? then do nothing.
+        if(playerDeck && computerDeck){
+            // if we are not currently in a war phase
+            if (warCount === 0){
+                // check if the player has lost
+                if (isGameOver(playerDeck)) {
+                    setText('You Lose!!!')
+                    setStopGame(true)
+                // check if the computer has lost
+                } else if (isGameOver(computerDeck)) {
+                    setText('You Won!')
+                    setStopGame(true)    
+                }
+            }
+        }
+
+        // extra phase if we have a war happening, get all the tribute cards set up
+        if (warCount > 0){
+            warPhase()
+        }
+
+        // happens everyround 
+        flipCards()
+
+        // find the winner of the Round, triggers a state change that should let the state for card slots update before checking the winner, which uses a state to decide
+        setCheckingWinner(!checkingWinner)
+    }
+
+    // start game
+    // start a round
+        // clear the board before a round
+        // check if each player has enough to deck cards to play
+        // is a war happening?
+            // yes? 
+            // war round
+        // check round winner
+        // add cards to deck
+        // update deck count
+        // end round
+
     return (
         <div>
+            {/* Is the game over? display this */}
             {stopGame ? 
                 <div>
                     {text}
                     <button onClick={startGame}>Play Again?</button> 
                 </div> : 
                 <div>
-                    {/* Computer Deck and Drawn Card */}
+                {/* Is the game still going ? display this */}
+
+                    {/* Computer Deck and Flipped Card */}
                     <div  className='flex'>
                         <div>
                             <div>Computer</div>
@@ -241,8 +283,8 @@ function War() {
                         }
                     </div>
                     
-                    {/* Computer card that started the war and tribute cards */}
-                    {warRoundCount > 0 && computerWarTribute !== undefined? 
+                    {/* Computer Tribute cards as long as we are in a war phase and we've added cards to the tribute decks */}
+                    {warCount > 0 && computerWarTribute !== undefined? 
                         <div className='war-tribute-board'>
                             {computerWarTribute.cards.map((card, i) => {
                                 return (
@@ -257,16 +299,29 @@ function War() {
                     {/* Text and Button */}
                     <div>
                         <div className='text'>{text}</div>
-                        {startWar ? 
-                            <button className='war-button' onClick={warRound}>Start War!</button> : 
-                            <button className='war-button' onClick={warRoundCount >= 2 ? flipCards : inRound ? cleanBeforeRound : flipCards}>
-                                {warRoundCount >= 2 ? 'Flip Cards' : inRound ? 'Clear' : 'Flip Cards'}
+                        {startNewWar ? 
+                            <button 
+                                className='war-button' 
+                                onClick={fullRound}
+                            > 
+                                Start War!
+                            </button> : 
+                            <button 
+                                className='war-button' 
+                                onClick={warCount > 1 ? 
+                                    fullRound : 
+                                    inRound ? 
+                                        cleanBeforeRound : 
+                                        fullRound
+                                }
+                            >
+                                {warCount > 1 ? 'Flip Cards' : inRound ? 'Clear' : 'Flip Cards'}
                             </button>
                         }
                     </div>
 
                     {/* Player War Tribute Deck */}
-                    {warRoundCount > 0 && playerWarTribute !== undefined? 
+                    {warCount > 0 && playerWarTribute !== undefined? 
                         <div className='war-tribute-board'>
                             {playerWarTribute.cards.map((card, i) => {
                                 return (
